9fb627cf11c3bea3264cdac14e10f1e7740bba22
Lokesh <lokesh@hipbar.com>
2019-09-27 21:32:34 +0530

    error message

--- core/src/main/java/com/elite/core/Util.kt
+++ core/src/main/java/com/elite/core/Util.kt
@@ -8,15 +8,19 @@ import java.lang.reflect.Type
 
 object Util {
     @Throws(EliteJsonException::class)
-    fun <R> parseJson(gson: Gson, code: Int,
-                      response: String, bodyType: Type): R? {
+    fun <R> parseJson(
+        gson: Gson, code: Int,
+        response: String, bodyType: Type
+    ): R? {
         try {
             return gson.fromJson<R>(response, bodyType)
         } catch (e: JsonSyntaxException) {
-            val msg = "FATAL : JSON structure not as expected. Schema changed maybe? : " + e.message
+//            val msg = "FATAL : JSON structure not as expected. Schema changed maybe? : " + e.message
+            val msg = "Something went wrong please try after sometime"
             throw EliteJsonException(code, msg, e)
         } catch (e: JsonParseException) {
-            val msg = "FATAL : Server didn't return valid JSON : " + e.message
+//            val msg = "FATAL : Server didn't return valid JSON : " + e.message
+            val msg = "Something went wrong please try after sometime"
             throw EliteJsonException(code, msg, e)
         }
     }
