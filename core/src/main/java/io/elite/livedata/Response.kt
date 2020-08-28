dd3e7eba34f13b71e8289bba910ed3606a96c36a
Lokesh <lokesh@hipbar.com>
2020-08-28 20:52:09 +0530

    update: Resource -> Response

--- core/src/main/java/io/elite/livedata/Response.kt
+++ core/src/main/java/io/elite/livedata/Response.kt
@@ -6,22 +6,22 @@ enum class Status {
     SUCCESS, FAILURE, LOADING, NO_INTERNET
 }
 
-class Resource<T> private constructor(val status: Status, val data: T?, val exception: AuthException?) {
+class Response<T> private constructor(val status: Status, val data: T?, val exception: AuthException?) {
     companion object {
-        fun <T> success(data: T?): Resource<T> {
-            return Resource(Status.SUCCESS, data, null)
+        fun <T> success(data: T?): Response<T> {
+            return Response(Status.SUCCESS, data, null)
         }
 
-        fun <T> failure(exception: AuthException?): Resource<T> {
-            return Resource(Status.FAILURE, null, exception)
+        fun <T> failure(exception: AuthException?): Response<T> {
+            return Response(Status.FAILURE, null, exception)
         }
 
-        fun <T> loading(): Resource<T> {
-            return Resource(Status.LOADING, null, null)
+        fun <T> loading(): Response<T> {
+            return Response(Status.LOADING, null, null)
         }
 
-        fun <T> noInternet(): Resource<T> {
-            return Resource(Status.NO_INTERNET, null, null)
+        fun <T> noInternet(): Response<T> {
+            return Response(Status.NO_INTERNET, null, null)
         }
     }
 }
